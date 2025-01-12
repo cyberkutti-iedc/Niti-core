dcf1bacf217be2d66fedb04bd9b2fb4292f2bf16
SREERAJ V RAJESH <119793289+cyberkutti-iedc@users.noreply.github.com>
2025-01-12 12:37:55 +0530

    Add files via upload

---
+++ niti-hal/src/port/mod.rs
@@ -0,0 +1,42 @@
+//! GPIO & Pin control.
+//!
+//! This module contains a [`Pins`] struct which represents all pins of the board.  The [`Pins`]
+//! struct is most easily constructed using the [`niti_hal::pins!()`][crate::pins] macro:
+//!
+//! ```no_run
+//! let dp = niti_hal::Peripherals::take().unwrap();
+//! let pins = niti_hal::pins!(dp);
+//! ```
+//!
+//! Additionally, the [`mode`] submodule contains all valid types for the `MODE` generic parameter
+//! of a pin.  The [`Pin`] type-alias represents a pin which can represent _any_ of the pins
+//! dynamically (while usually each pin has its own type).
+//!
+//! Check the documentation for [`avr_hal_generic::port::Pin`] for a detailed explanation of GPIO
+//! pins in `avr-hal`.
+
+
+
+
+#[cfg(feature = "arduino-leonardo")]
+pub use leonardo::*;
+#[cfg(any(feature = "arduino-mega2560", feature = "arduino-mega1280"))]
+mod mega;
+#[cfg(any(feature = "arduino-mega2560", feature = "arduino-mega1280"))]
+pub use mega::*;
+#[cfg(any(
+    feature = "arduino-nano",
+    feature = "arduino-uno",
+    feature = "nano168",
+ \
+))]
+mod uno;
+#[cfg(any(
+    feature = "arduino-nano",
+    feature = "arduino-uno",
+    feature = "nano168",
+
+))]
+pub use uno::*;
+
+
